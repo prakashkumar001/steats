aeaf6fc56682c2be264b94f58088ffed16c0ab14
unknown <akshav00@gmail.com>
2018-08-08 01:04:44 -0700

    change

---
+++ app/src/main/java/street/com/eats/fragment/ChangePassword.java
@@ -0,0 +1,22 @@
+package street.com.eats.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import street.com.eats.R;
+
+public class ChangePassword extends Fragment
+{
+
+    @Override
+public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    View view=inflater.inflate(R.layout.changepassword, container, false);
+
+
+
+    return view;
+}
+}
