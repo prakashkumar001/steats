b94851bbd1f4904b2a3fe78c23fa4572ea8bcdfc
Prakash A <prakash@dexeldesigns.com>
2018-08-14 15:00:41 +0530

    showcase

--- app/src/main/java/street/com/eats/fragment/HotelDashboard.java
+++ app/src/main/java/street/com/eats/fragment/HotelDashboard.java
@@ -1,6 +1,14 @@
 package street.com.eats.fragment;
 
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
@@ -23,7 +31,12 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+
+import com.github.amlcurran.showcaseview.ShowcaseDrawer;
+import com.github.amlcurran.showcaseview.ShowcaseView;
+import com.github.amlcurran.showcaseview.targets.ViewTarget;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -41,19 +54,48 @@ import street.com.eats.common.Snap;
  * Created by Creative IT Works on 09-Jul-18.
  */
 
-public class HotelDashboard extends Fragment{
+public class HotelDashboard extends Fragment {
+
     RecyclerView recyclerView;
     List<Hotel> hotelList;
     public  FrameLayout frame;
     public static final String ORIENTATION = "orientation";
     ImageView back;
     private boolean mHorizontal;
+    ShowcaseView sv;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view=inflater.inflate(R.layout.hotel_dashboard, container, false);
+        final View view=inflater.inflate(R.layout.hotel_dashboard, container, false);
 
         intailiseView(view);
+
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                ViewTarget target = new ViewTarget(view.findViewById(R.id.back));
+
+
+             /*   new ShowcaseView.Builder(getActivity())
+                        .setTarget(new ViewTarget(R.id.back, getActivity()))
+                        .setContentTitle("Its My Navigation Drawer")
+                        .setContentText("Click here and you will get options to navigate to other sections.")
+                        .setShowcaseDrawer(new CustomShowcaseView(getResources()))
+                        .build();*/
+
+                new ShowcaseView.Builder(getActivity())
+                        .withMaterialShowcase()
+                        .setStyle(R.style.CustomShowcaseTheme2)
+                        .setTarget(new ViewTarget(R.id.back, getActivity()))
+                        .hideOnTouchOutside()
+                        .setContentTitle("Its My Navigation Drawer")
+                        .setContentText("Click here and you will get options to navigate to other sections.")
+                        .build();
+            }
+        },1000);
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -111,6 +153,75 @@ public class HotelDashboard extends Fragment{
         return apps;
     }
 
+    private static class CustomShowcaseView implements ShowcaseDrawer {
+
+        private final float width;
+        private final float height;
+        private final Paint eraserPaint;
+        private final Paint basicPaint;
+        private final int eraseColour;
+        private final RectF renderRect;
+
+        public CustomShowcaseView(Resources resources) {
+            width = resources.getDimension(R.dimen.custom_showcase_width);
+            height = resources.getDimension(R.dimen.custom_showcase_height);
+            PorterDuffXfermode xfermode = new PorterDuffXfermode(PorterDuff.Mode.MULTIPLY);
+            eraserPaint = new Paint();
+            eraserPaint.setColor(0xFFFFFF);
+            eraserPaint.setAlpha(0);
+            eraserPaint.setXfermode(xfermode);
+            eraserPaint.setAntiAlias(true);
+            eraseColour = Color.parseColor("#50FF562F");
+            basicPaint = new Paint();
+            renderRect = new RectF();
+        }
+
+        @Override
+        public void setShowcaseColour(int color) {
+            eraserPaint.setColor(color);
+        }
+
+        @Override
+        public void drawShowcase(Bitmap buffer, float x, float y, float scaleMultiplier) {
+            Canvas bufferCanvas = new Canvas(buffer);
+            renderRect.left = x - width / 2f;
+            renderRect.right = x + width / 2f;
+            renderRect.top = y - height / 2f;
+            renderRect.bottom = y + height / 2f;
+            bufferCanvas.drawRect(renderRect, eraserPaint);
+        }
+
+        @Override
+        public int getShowcaseWidth() {
+            return (int) width;
+        }
+
+        @Override
+        public int getShowcaseHeight() {
+            return (int) height;
+        }
+
+        @Override
+        public float getBlockedRadius() {
+            return width;
+        }
+
+        @Override
+        public void setBackgroundColour(int backgroundColor) {
+            // No-op, remove this from the API?
+        }
+
+        @Override
+        public void erase(Bitmap bitmapBuffer) {
+            bitmapBuffer.eraseColor(eraseColour);
+        }
+
+        @Override
+        public void drawToCanvas(Canvas canvas, Bitmap bitmapBuffer) {
+            canvas.drawBitmap(bitmapBuffer, 0, 0, basicPaint);
+        }
+
+    }
 
 
 }
